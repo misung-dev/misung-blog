efc68e5e71051aa35ca0a511847bf8eb5021ab42
misung.dev <misung.dev@gmail.com>
2025-08-04 22:46:06 +0900

    feat: ESLint 설정

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import globals from "globals";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +12,21 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  js.configs.recommended,
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      // 여기에 추가적인 규칙을 설정할 수 있습니다
+      "no-unused-vars": "warn",
+      "no-console": "warn",
+    },
+  },
 ];
 
 export default eslintConfig;
