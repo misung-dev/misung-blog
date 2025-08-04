a9ace0354423b64ac6adcc18686062c510d9821d
misung.dev <misung.dev@gmail.com>
2025-08-04 23:23:03 +0900

    refactor: Header, Footer 컴포넌트 분리

---
+++ components/layouts/Header.tsx
@@ -0,0 +1,22 @@
+export default function Header() {
+  return (
+    <header className="sticky top-0 z-50 border-b">
+      <div className="container mx-auto flex h-14 items-center px-4">
+        <a href="#" className="text-xl font-semibold">
+          <span className="font-bold">미숑이의 블로그</span>
+        </a>
+        <nav className="ml-auto flex items-center gap-4">
+          <a href="#" className="hover:text-primary font-medium">
+            홈
+          </a>
+          <a href="#" className="hover:text-primary font-medium">
+            블로그
+          </a>
+          <a href="#" className="hover:text-primary font-medium">
+            소개
+          </a>
+        </nav>
+      </div>
+    </header>
+  );
+}
