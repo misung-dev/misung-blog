4addca0b0faa3b9de311cc6016995467362e8e0e
misung.dev <misung.dev@gmail.com>
2025-08-05 17:36:38 +0900

    setting: @next/mdx 패키지 설치 및 next.config.mjs 설정

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from 'next';
+import createMDX from '@next/mdx';
 
 const nextConfig: NextConfig = {
   images: {
@@ -14,6 +15,9 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
 };
 
-export default nextConfig;
+const withMDX = createMDX({});
+
+export default withMDX(nextConfig);
